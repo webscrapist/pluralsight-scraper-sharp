a89d3d90242f20473c6c57a848080e2e8ad526aa
Santiago Vasquez <santiago@vasquezhouse.com>
2019-03-24 20:37:41 -0400

    removed previous PoC

--- pluralsight-scraper/Program.cs
+++ pluralsight-scraper/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace VH.PluralsightScraper
+﻿using System;
+
+namespace VH.PluralsightScraper
 {
     internal class Program
     {
         private static void Main()
         {
-            new PsScraper().Start();
+            Console.WriteLine("hi .net core 2.2");
         }
     }
 }
