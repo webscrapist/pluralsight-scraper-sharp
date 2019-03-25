db041e4a2d11748bce850a9666e3d8996d55e5a5
Santiago Vasquez <santiago@vasquezhouse.com>
2019-03-24 22:05:45 -0400

    Implemented Puppeteer PoC

--- pluralsight-scraper/Program.cs
+++ pluralsight-scraper/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading.Tasks;
+using PuppeteerSharp;
 
 namespace VH.PluralsightScraper
 {
@@ -6,7 +8,27 @@ namespace VH.PluralsightScraper
     {
         private static void Main()
         {
-            Console.WriteLine("hi .net core 2.2");
+            if (TakeScreenshot().Result)
+            {
+                Console.WriteLine("all good");
+            }
+        }
+
+        private static async Task<bool> TakeScreenshot()
+        {
+            await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
+
+            Browser browser = await Puppeteer.LaunchAsync(new LaunchOptions { Headless = true });
+
+            Page page = await browser.NewPageAsync();
+
+            await page.GoToAsync("http://www.google.com");
+
+            const string OUTPUT_FILE = @"C:\Users\Santiago\Desktop\delete\scraper-output\pic.jpg";
+
+            await page.ScreenshotAsync(OUTPUT_FILE);
+
+            return true;
         }
     }
 }
