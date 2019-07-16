b09896d5797f5c1b7bb05bb3f70a019eebbb3140
Santiago Vasquez <santiago@vasquezhouse.com>
2019-07-15 22:11:59 -0400

    added initial migration

---
+++ pluralsight-scraper/Migrations/20190716021015_InitialCreate.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace VH.PluralsightScraper.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "public");
+
+            migrationBuilder.CreateTable(
+                name: "Channels",
+                schema: "public",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Name = table.Column<string>(maxLength: 100, nullable: false),
+                    Url = table.Column<string>(maxLength: 100, nullable: false),
+                    CreatedAtUTC = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 50, nullable: false),
+                    LastUpdatedAtUTC = table.Column<DateTime>(nullable: true),
+                    LastUpdatedBy = table.Column<string>(maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Channels", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                schema: "public",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Name = table.Column<string>(maxLength: 100, nullable: false),
+                    Level = table.Column<int>(nullable: false),
+                    DatePublished = table.Column<DateTime>(nullable: false),
+                    CreatedAtUTC = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 50, nullable: false),
+                    LastUpdatedAtUTC = table.Column<DateTime>(nullable: true),
+                    LastUpdatedBy = table.Column<string>(maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ChannelCourse",
+                schema: "public",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    ChannelId = table.Column<int>(nullable: false),
+                    CourseId = table.Column<int>(nullable: false),
+                    CreatedAtUTC = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 50, nullable: false),
+                    LastUpdatedAtUTC = table.Column<DateTime>(nullable: true),
+                    LastUpdatedBy = table.Column<string>(maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChannelCourse", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ChannelCourse_Channels_ChannelId",
+                        column: x => x.ChannelId,
+                        principalSchema: "public",
+                        principalTable: "Channels",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ChannelCourse_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalSchema: "public",
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChannelCourse_ChannelId",
+                schema: "public",
+                table: "ChannelCourse",
+                column: "ChannelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChannelCourse_CourseId",
+                schema: "public",
+                table: "ChannelCourse",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Channels_Name",
+                schema: "public",
+                table: "Channels",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_Name",
+                schema: "public",
+                table: "Courses",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChannelCourse",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "Channels",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "Courses",
+                schema: "public");
+        }
+    }
+}
