f56b27593ac98940f419971832e632adf66190e5
Santiago Vasquez <santiago@vasquezhouse.com>
2019-07-15 23:25:59 -0400

    created migration for course date published nullable

---
+++ pluralsight-scraper/Migrations/20190716032256_CourseDatePublishedNullable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VH.PluralsightScraper.Migrations
+{
+    public partial class CourseDatePublishedNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DatePublished",
+                schema: "public",
+                table: "Courses",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DatePublished",
+                schema: "public",
+                table: "Courses",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+        }
+    }
+}
